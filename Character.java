7bebf455d5cfcf8c5550e5ec7e25b19433e75c0f
darien-sweitzer <43593384+darien-sweitzer@users.noreply.github.com>
2018-11-07 13:50:14 -0500

    Add files via upload

---
+++ Character.java
@@ -0,0 +1,20 @@
+package Game;
+
+
+
+public class Character {
+
+    // to/do: add armor into Battle
+    
+    String role;
+    String name;
+    int health;
+    Abilities abilityobject;
+    
+    public Character(Abilities ab) {
+        abilityobject = ab;
+        
+        
+    }
+    
+} // close class
