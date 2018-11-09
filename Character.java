b17d1709019961fc4977dff8ba1d2b852a2a3c41
darien-sweitzer <43593384+darien-sweitzer@users.noreply.github.com>
2018-11-09 16:28:21 -0500

    Add files via upload

---
+++ Character.java
@@ -0,0 +1,17 @@
+
+package Game;
+
+
+
+public class Character {
+
+    String role;
+    String name;
+    int health;
+    Abilities abilityobject;
+
+    public void Character(Abilities ab) {
+        abilityobject = ab;
+    }
+    
+} // close class
