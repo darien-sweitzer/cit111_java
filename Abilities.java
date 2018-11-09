1233fb69307bd7f28beeb665d9507d3e17fee66d
darien-sweitzer <43593384+darien-sweitzer@users.noreply.github.com>
2018-11-09 16:23:52 -0500

    Add files via upload

---
+++ Abilities.java
@@ -0,0 +1,12 @@
+package Game;
+
+public class Abilities {
+
+    public String ability;
+    public String ability2;
+    public String ability3;
+    public int damage;
+    public int damage2;
+    public int damage3;
+    
+} // close Class
